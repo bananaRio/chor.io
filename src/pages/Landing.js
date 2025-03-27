65cc801ac5ee86c3bd62cce35e830240b20c7b87
Max Huang <maxyuzehuang@gmail.com>
2025-03-27 16:14:00 -0500

    2 test pages

---
+++ src/pages/Landing.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Landing(){
+    return(
+        <div>
+            <h1>Landing</h1>
+            <button className="botContentButton" type="button">
+                <a style={{color:"black"}}href="/routineHome">Test</a>
+            </button>
+        </div>
+    );
+}
+
+export default Landing;
---
+++ src/pages/routineHome.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function rHome(){
+    return(
+        <div>
+            <h1>Routine</h1>
+            <button className="botContentButton" type="button">
+                <a style={{color:"black"}}href="/">Back</a>
+            </button>
+        </div>
+    );
+}
+
+export default rHome;
